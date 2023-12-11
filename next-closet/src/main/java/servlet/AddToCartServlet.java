607216c7db3dd51624cab96a7f4b1c928f41aabc
TaigaShiokawa <148533570+TaigaShiokawa@users.noreply.github.com>
2023-12-11 15:16:48 +0900

    アノテーション変更

--- next-closet/src/main/java/servlet/AddToCartServlet.java
+++ next-closet/src/main/java/servlet/AddToCartServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("AddToCartServlet")
+@WebServlet("/AddToCartServlet")
 public class AddToCartServlet extends HttpServlet  {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
