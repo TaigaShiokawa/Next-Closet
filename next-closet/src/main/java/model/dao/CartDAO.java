f113a05333b8b4ec044ff4f11669e0cd86a5cb22
shunya-kimura <148534097+shunya-kimura@users.noreply.github.com>
2023-12-12 15:43:45 +0900

    カート一覧機能(途中)

--- next-closet/src/main/java/model/dao/CartDAO.java
+++ next-closet/src/main/java/model/dao/CartDAO.java
@@ -2,11 +2,16 @@ package model.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
 import connection.DBConnection;
+import model.bean.CartItemBean;
+import model.bean.ProductBean;
 
 public class CartDAO {
 	
@@ -25,4 +30,39 @@ public class CartDAO {
 		}
 		return processingNum;
 	}
+	
+	//ユーザーの買い物かご一覧を表示する処理
+	public List<CartItemBean> getCartItems(int userId) 
+			throws ClassNotFoundException, SQLException {
+		List<CartItemBean> cartItems = new ArrayList<>();
+		
+		String sql ="SELECT ci.cart_item_id, ci.quantity, p.product_name, p.price, p.image, s.size_name"
+				+ "FROM cart_items ci"
+				+ "INNER JOIN products p ON ci.product_id = p.product_id"
+				+ "INNER JOIN sizes s ON ci.size_id = s.size_id"
+				+ "WHERE ci.user_id = ?";
+		
+		try (Connection con = DBConnection.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql)) {
+			pstmt.setInt(1, userId);
+			try (ResultSet res = pstmt.executeQuery()) {
+				while (res.next()) {
+					CartItemBean cartItem = new CartItemBean();
+					cartItem.setCartItemId(res.getInt("cart_item_id"));
+					cartItem.setQuantity(res.getInt("quantity"));
+					
+					ProductBean product = new ProductBean();
+					
+					
+					
+				}
+			}
+		}
+		return cartItems;
+	}
+	
+	
+	
+	
+	
 }
