d3b19be242d98a1daffe32d40aee5ea74dd34cb8
KishimoyoMiyu <148535550+KishimoyoMiyu@users.noreply.github.com>
2023-12-13 23:10:46 +0900

    ストック管理

--- next-closet/src/main/java/model/StockManager.java
+++ next-closet/src/main/java/model/StockManager.java
@@ -6,10 +6,11 @@ import model.dao.ProductDAO;
 
 public class StockManager {
 	
-	//単品の商品
+		ProductDAO dao = new ProductDAO();
+	
+	//単品の商品の在庫と購入数を比較して、たりたらtrue たりなかったらfalse
 	public boolean productStock(int productId , int sizeId , int quantity)  {
 		
-		ProductDAO dao = new ProductDAO();
 		boolean stock = true;
 		int stockQuantity = 0;
 		try {
@@ -23,6 +24,20 @@ public class StockManager {
 		}
 		
 		return stock;
+	}
+	
+	//在庫数 - 購入数　を　在庫数に更新するメソッド
+	public void decrementStock(int productId , int sizeId , int buyQuantity , String delivery_address) {
+		
+		int  nowStockQuantity = -1;
+		try {
+			 nowStockQuantity = dao.nowStockQuantity(productId , sizeId );
+			 int quantity = nowStockQuantity - buyQuantity;
+			 dao.changeStock( quantity , productId , sizeId , delivery_address);
+		} catch (SQLException | ClassNotFoundException e) {
+				e.printStackTrace();
+		}
+	
 	}
 	
 	
